79476f49b1b5d199d6c3eee0149570c7a976e1f1
tanmaythole <tanmaythole@gmail.com>
2021-08-30 10:16:07 +0530

    style fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/business">Business</Link>
@@ -44,7 +44,7 @@ export default function Navbar(props) {
                             aria-label="Search" 
                         />
                         <Link to={`/${query}`}>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <button className="btn btn-success" type="submit">Search</button>
                         </Link>
                     </form>
                 </div>
