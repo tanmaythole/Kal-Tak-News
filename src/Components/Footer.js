ef0ecb82c4ba5f5d5ee8c0392012d7c84d1cd852
Tanmay Thole <tanmaythole@gmail.com>
2022-03-11 09:56:28 +0530

    Image Size Fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div>
             <div className="bg-dark text-center py-4 mt-5">
                 <p className="m-0 text-light">Copyright &copy; 2021, Kal Tak News</p>
-                <p className="m-0 text-muted">Designed & Developed with ❤️ by <a href="https://tanmaythole.github.io/portfolio/" target="_blank" className="text-primary text-decoration-none text-bold">Tanmay Thole</a></p>
+                <p className="m-0 text-muted">Designed & Developed with ❤️ by <a href="https://tanmaythole.github.io/portfolio/" target="_blank" rel='noreferrer' className="text-primary text-decoration-none text-bold">Tanmay Thole</a></p>
             </div>
         </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -4,7 +4,7 @@ export default function NewsItem({title, desc, imageURL, url, author, date, sour
     return (
         <div>
             <div className="card border-0" style={{height:"100%"}}>
-                <img src={imageURL?imageURL:"/logo.png"} className="card-img-top" style={!imageURL?{background:"black", padding:"50px 20px"}:{}} alt="..." />
+                <img src={imageURL?imageURL:"/logo.png"} className="card-img-top" style={!imageURL?{background:"black", padding:"50px 20px"}:{height: "200px"}} alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <span className="position-absolute top-0 px-2 bg-danger badge rounded text-light" style={{right:"0%", fontSize:"12px", zIndex:"1"}}>
